8c416d65d1a0a12b40c4222e81c87b22ca492fe1
Amolghegad1234 <121895274+Amolghegad1234@users.noreply.github.com>
2023-01-07 15:14:17 +0530

    second commit

--- src/sample1/class1.java
+++ src/sample1/class1.java
@@ -14,6 +14,7 @@ public class class1
 		arr[2]=30;
 		arr[3]=40;
 		arr[4]=50;
+		arr[5]=60;
 		
 		//Step 3 Array usage
 		//using index
@@ -22,6 +23,7 @@ public class class1
 		System.out.println(arr[2]);
 		System.out.println(arr[3]);
 		System.out.println(arr[4]);
+		System.out.println(arr[5]);
 		System.out.println("*********");
 		
 		//using foreach loop
